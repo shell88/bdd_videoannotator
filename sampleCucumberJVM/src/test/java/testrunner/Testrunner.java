bd31a9d2930e7c91f3dff1b83ecdf7be04b8b059
shell88 <stefan88hell@gmail.com>
2015-07-04 09:43:01 +0200

    Path refactored

--- sampleCucumberJVM/src/test/java/testrunner/Testrunner.java
+++ sampleCucumberJVM/src/test/java/testrunner/Testrunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = "com.github.shell88.bddvideoannotator.javaadapters.CucumberReportingAdapter", features = { "src/features" }, glue = "stepdefinition")
+@CucumberOptions(plugin = "com.github.shell88.bddvideoannotator.javaadapters.CucumberReportingAdapter", features = { "src/test/java/features" }, glue = "stepdefinition")
 public class Testrunner {
 
 
