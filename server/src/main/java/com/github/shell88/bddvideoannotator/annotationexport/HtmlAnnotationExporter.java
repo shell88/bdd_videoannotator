d105ae01447b7ad316cbf1818ab28dce59e67810
shell88 <stefan88hell@gmail.com>
2015-07-29 16:00:10 +0200

    Copy of HTML to outputFile and data Replacement

--- server/src/main/java/com/github/shell88/bddvideoannotator/annotationexport/HtmlAnnotationExporter.java
+++ server/src/main/java/com/github/shell88/bddvideoannotator/annotationexport/HtmlAnnotationExporter.java
@@ -1,19 +1,76 @@
 package com.github.shell88.bddvideoannotator.annotationexport;
 
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HtmlAnnotationExporter extends AnnotationExporter {
 
+  private final String replaceIdentifierScenarioName = "#scenarioName";
+  private final String replaceIdentifierSteps = "#steps";
+  private final String replaceIdentifierVideoRef = "#video";
+
+  private final String packagePrefix = "/com/github/shell88/bddvideoannotator/annotationexport/html";
+  private final String[] assets = new String[] { "index.html",
+      "angular.min.js", "bootstrap.min.css" };
+
+  private List<StepAnnotation> steps;
+
+  public HtmlAnnotationExporter() {
+    steps = new ArrayList<StepAnnotation>();
+  }
 
   @Override
   public void addStepAnnotation(StepAnnotation step) {
-      
+    steps.add(step);
   }
-
 
   @Override
-  public void endOfCurrentScenario(String scenarioName, String pathToVideoFile, String checksum) throws Exception {
-    // TODO Auto-generated method stub
+  public void endOfCurrentScenario(String scenarioName, String pathToVideoFile,
+      String checksum) throws Exception {
+
+    copyAssetsToOutputDirectory();
+    
+    //Replacements
+    File indexFileCopy = new File(getOutputDirectory(), "index.html");
+    String content = FileUtils.readFileToString(indexFileCopy, "UTF-8");
+    //TODO: implement with GSON
+    //TODO: FileUtils in maven shade plugin
+    content = content.replaceFirst(replaceIdentifierScenarioName, "'" + scenarioName + "'");
+    content = content.replaceFirst(replaceIdentifierVideoRef, "'" + pathToVideoFile + "'");
+
+    String stepJSONString = "";
+    for (StepAnnotation step : steps) {
+      stepJSONString += "{ 'starttime': " + step.getMillisecondsFrom()
+          + ", 'endtime': " + step.getMillisecondsTo() + "}";
+    }
+    content = content.replaceFirst(replaceIdentifierSteps, stepJSONString);
+    FileUtils.writeStringToFile(indexFileCopy, content);
+    // String fileContent =
+
   }
+  
+  private void copyAssetsToOutputDirectory() {
+
+    for (String assetName : assets) {
+      InputStream assetStream = getClass().getResourceAsStream(
+          packagePrefix + "/" + assetName);
+      if (assetStream == null) {
+        // TODO use right exception type
+        throw new RuntimeException("Could not find: " + assetName);
+      }
+      try {
+        FileUtils.copyInputStreamToFile(assetStream,
+            new File(this.getOutputDirectory(), assetName));
+      } catch (IOException e) {
+        throw new RuntimeException("Unable to write to report file item: ", e);
+      }
+    }
+  }
+  
 
 }
